61cdaa1c247964bd1ec5acc2686b09fcd489a2b2
Sharif H. Adal <mdadalbadsha2008@gmail.com>
2025-12-17 13:04:59 +0600

    add localhost development URL to allowed origins

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(process.env.DB_URI, {
 const app = express();
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     optionSuccessStatus: 200,
   })
