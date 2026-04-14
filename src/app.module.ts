8bbd3141e9c013cb6b4f631c48b0c75d814b5bd8
Sharif Adal <sharifadal2008@gmail.com>
2026-04-14 20:24:04 +0600

    feat: implement user module and integrate with authentication service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { AuthService } from './auth/auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  registerUser() {
+    // Implementation for user registration
+    this.userService.createUser();
+  }
+}
